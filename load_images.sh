5f0d9def735dc90d26826d5e0562d7ec0228b30e
Freyman Balaguera <freybalaguera@gmail.com>
2021-10-17 23:03:53 -0500

    added db to microservices

--- load_images.sh
+++ load_images.sh
@@ -1,14 +1,28 @@
-eval $(minikube docker-env)
+#!/bin/bash
 
-docker image prune -y
+#eval $(minikube docker-env)
 
+docker container stop accounts
+docker container stop auths
+docker container stop people
+docker container stop mysql
+
+docker network prune -f
+docker container prune -f 
+docker image prune -f
 
 docker build -t auth ./auth
-
+docker build -t accounts ./accounts
 docker build -t people ./people
 
-docker build -t accounts ./accounts
+docker run --detach --name accounts --env-file env/variables -p 8081:8080  accounts
+docker run --detach --name auths --env-file env/variables -p 8082:8080 auth
+docker run --detach --name people --env-file env/variables  -p 8083:8080 people
+docker run --detach --name mysql -p 3306:3306 --env-file env/variables mysql:latest
 
 docker images
+docker ps --all
 
-eval $(minikube docker-env -u)
+python3 -m http.server
+
+#eval $(minikube docker-env -u)
